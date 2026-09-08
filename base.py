e88dd23d6515005d0b3596b83f1fde3e910637b9
BakedPotato179 <olliepenrod@gmail.com>
2026-09-08 11:38:23 -0500

    Added initial questionnaire

--- base.py
+++ base.py
@@ -4,3 +4,17 @@ By Ben, Garrett, and Ollie
 We're going to do a Jeopardy style quiz, where you answer questions from categories you've selected to earn a total of $(specify amount)
 """
 
+print("Welcome to Jeopardy! Your goal is to collect $(unspecified) by answering questions correctly! Your categories are")
+print("Wonders")
+print("Food")
+print("Books")
+print("Please type your category exactly. Capitalization matters!")
+cat_select: str = input()
+if cat_select == "Wonders":
+    print("You've selected Wonders!")
+elif cat_select == "Food":
+    print("You've selected Food!")
+elif cat_select == "Books":
+    print("You've selected Books!")
+else:
+    print("Please choose a category!")
