bf802363d820566ead0c8750e5db6f904b89971a
Ben Johnson <johben17796@gmail.com>
2026-09-10 11:03:36 -0500

    Added function to decrement the total if the ans is wrong

--- base.py
+++ base.py
@@ -51,7 +51,8 @@ match category:
         question: tuple = wonders.get(dollars)
         answer: str = input(question[0] + " ")
         if answer == question[1]:
-            money += int(dollars)
+            money += int(dollars) # increments the amount if correct
+        else: money -= int(dollars) # and decrements if wrong.
             print(f"Correct! You now have {dollars} dollars.")
     # Fill in these other 2 later
     case "Food":
